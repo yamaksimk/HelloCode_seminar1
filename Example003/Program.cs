014af19d34b957988be83fe192c96c19c1134824
yamaksimk <mkurakin2001@gmail.com>
2023-09-30 21:44:31 +0300

    Решил задачу с последней цифрой в трехзначном числе

---
+++ Example003/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Последняя цифра числа: " + num % 10); // %10 - остаток для трехзначного числа
