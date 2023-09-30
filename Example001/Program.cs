e69c1ff41d2c3623fce52e04310d0190d0121fd5
yamaksimk <mkurakin2001@gmail.com>
2023-09-30 21:18:49 +0300

    Выполнил первую задачу с днями недели

---
+++ Example001/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введите номер дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("Это понедельник");
+}
+
+else if (a == 2)
+{
+    Console.WriteLine("Это вторник");
+}
+
+else if (a == 3)
+{
+    Console.WriteLine("Это среда");
+}
+
+else if (a == 4)
+{
+    Console.WriteLine("Это четверг");
+}
+
+else if (a == 5)
+{
+    Console.WriteLine("Это пятница");
+}
+
+else if (a == 6)
+{
+    Console.WriteLine("Это суббота");
+}
+
+else if (a == 7)
+{
+    Console.WriteLine("Это воскресенье");
+}
+
+else
+{
+    Console.WriteLine("Это не день недели");
+}
