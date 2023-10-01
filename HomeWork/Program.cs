260d4854684febe5af5e1ddeb88f1a26ece104d9
yamaksimk <mkurakin2001@gmail.com>
2023-10-01 15:58:58 +0300

    Сделал домашнее задание

---
+++ HomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine("Число " + a + " больше чем " + b);
+else if (a == b)
+    Console.WriteLine("Эти числа равны");
+else
+    Console.WriteLine("Число " + b + " больше чем " + a); */
+
+
+
+/*Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(a > max) max = a;
+if (b > max) max = b;
+if(c > max) max = c;
+Console.WriteLine("Максимальное число из этих чисел - " + max); */
+
+
+
+/* Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 >0)  
+    Console.WriteLine("Число нечетное"); 
+else
+    Console.WriteLine("Число четное"); */
+
+
+
+/* Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
+for (int i = 1; i <= N; i++)
+Console.Write(i + " ");
+else
+    Console.WriteLine("Число должно быть положительным!"); */
